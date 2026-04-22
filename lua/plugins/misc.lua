efa062d352dd5341a25e1622fc408326469a0ea7
Peylics <lisichen_2901@outlook.com>
2026-04-22 07:08:18 -0400

    Add im-select.nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -5,3 +5,17 @@ vim.pack.add({
 })
 
 require("buffer_manager").setup({})
+
+-- im-select.nvim
+-- must have macism installed
+if Config.profile_is_default and vim.fn.has("mac") == 1 then
+  vim.pack.add({ "https://github.com/keaising/im-select.nvim" })
+
+  require("im_select").setup({
+    default_im_select = "com.apple.keylayout.US",
+    default_command = "macism",
+    set_default_events = { "InsertLeave", "CmdlineLeave", "FocusGained" },
+    set_previous_events = { "InsertEnter" },
+    async_switch_im = true,
+  })
+end
