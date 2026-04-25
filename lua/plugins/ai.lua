6aad542a689d9b50b142a0e699faa874c987421f
Peylics <lisichen_2901@outlook.com>
2026-04-25 00:42:34 -0400

    Disable copilot related stuff

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,52 +3,55 @@ if Config.profile_is_reduced then return {} end
 local map = vim.keymap.set
 
 -- copilot.lua
-vim.pack.add({
-  "https://github.com/copilotlsp-nvim/copilot-lsp",
-  "https://github.com/zbirenbaum/copilot.lua",
-})
-
-require("copilot").setup({
-  suggestion = {
-    enabled = not vim.g.ai_cmp,
-    auto_trigger = true,
-    hide_during_completion = vim.g.ai_cmp,
-    keymap = {
-      accept = "<M-l>", -- handled by nvim-cmp / blink.cmp
-      next = "<M-]>",
-      prev = "<M-[>",
-    },
-  },
-  panel = { enabled = false },
-  filetypes = {
-    markdown = true,
-    help = true,
-  },
-})
-
-Config.create_autocmd("User", "BlinkCmpMenuOpen", function()
-  vim.b.copilot_suggestion_hidden = true
-end)
-
-Config.create_autocmd("User", "BlinkCmpMenuClose", function()
-  vim.b.copilot_suggestion_hidden = false
-end)
+-- vim.pack.add({
+--   "https://github.com/copilotlsp-nvim/copilot-lsp",
+--   "https://github.com/zbirenbaum/copilot.lua",
+-- })
+--
+-- require("copilot").setup({
+--   suggestion = {
+--     enabled = not vim.g.ai_cmp,
+--     auto_trigger = true,
+--     hide_during_completion = vim.g.ai_cmp,
+--     keymap = {
+--       accept = "<M-l>", -- handled by nvim-cmp / blink.cmp
+--       next = "<M-]>",
+--       prev = "<M-[>",
+--     },
+--   },
+--   panel = { enabled = false },
+--   filetypes = {
+--     markdown = true,
+--     help = true,
+--   },
+--   disable_limit_reached_message = true,
+-- })
+--
+-- Config.create_autocmd("User", "BlinkCmpMenuOpen", function()
+--   vim.b.copilot_suggestion_hidden = true
+-- end)
+--
+-- Config.create_autocmd("User", "BlinkCmpMenuClose", function()
+--   vim.b.copilot_suggestion_hidden = false
+-- end)
 
 -- sidekick.nvim
 vim.pack.add({ "https://github.com/folke/sidekick.nvim" })
 
 require("sidekick").setup()
 
-map({ "i", "n" }, "<Tab>", function()
-  -- If there is a next edit, jump to it or apply it.
-  if require("sidekick").nes_jump_or_apply() then return "" end
+require("sidekick.nes").disable()
 
-  -- Fallback to a literal Tab.
-  return "<tab>"
-end, {
-  expr = true,
-  desc = "Goto/Apply Next Edit Suggestion",
-})
+-- map({ "i", "n" }, "<Tab>", function()
+--   -- If there is a next edit, jump to it or apply it.
+--   if require("sidekick").nes_jump_or_apply() then return "" end
+--
+--   -- Fallback to a literal Tab.
+--   return "<tab>"
+-- end, {
+--   expr = true,
+--   desc = "Goto/Apply Next Edit Suggestion",
+-- })
 
 map({ "n", "t", "i", "x" }, "<C-.>", function()
   require("sidekick.cli").focus()
