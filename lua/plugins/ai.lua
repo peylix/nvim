e30329f700ffdae34ae23068916fa1db98c64a9d
Peylics <lisichen_2901@outlook.com>
2026-05-17 16:16:12 -0400

    Re-enable copilot.lua

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -2,45 +2,48 @@ if Config.profile_is_reduced then return {} end
 
 local map = vim.keymap.set
 local add = vim.pack.add
+local autocmd = vim.api.nvim_create_autocmd
 
 -- copilot.lua
--- add({
---   Config.gh("copilotlsp-nvim/copilot-lsp"),
---   Config.gh("zbirenbaum/copilot.lua"),
--- })
---
--- require("copilot").setup({
---   suggestion = {
---     enabled = not vim.g.ai_cmp,
---     auto_trigger = true,
---     hide_during_completion = vim.g.ai_cmp,
---     keymap = {
---       accept = "<M-l>", -- handled by nvim-cmp / blink.cmp
---       next = "<M-]>",
---       prev = "<M-[>",
---     },
---   },
---   panel = { enabled = false },
---   filetypes = {
---     markdown = true,
---     help = true,
---   },
---   disable_limit_reached_message = true,
--- })
---
--- vim.api.nvim_create_autocmd("BlinkCmpMenuOpen", {
---   group = Config.augr,
---   callback = function()
---     vim.b.copilot_suggestion_hidden = true
---   end,
--- })
---
--- vim.api.nvim_create_autocmd("BlinkCmpMenuClose", {
---   group = Config.augr,
---   callback = function()
---     vim.b.copilot_suggestion_hidden = false
---   end,
--- })
+add({
+  Config.gh("copilotlsp-nvim/copilot-lsp"),
+  Config.gh("zbirenbaum/copilot.lua"),
+})
+
+require("copilot").setup({
+  suggestion = {
+    enabled = not vim.g.ai_cmp,
+    auto_trigger = true,
+    hide_during_completion = vim.g.ai_cmp,
+    keymap = {
+      accept = "<M-l>", -- handled by nvim-cmp / blink.cmp
+      next = "<M-]>",
+      prev = "<M-[>",
+    },
+  },
+  panel = { enabled = false },
+  filetypes = {
+    markdown = true,
+    help = true,
+  },
+  disable_limit_reached_message = true,
+})
+
+autocmd("User", {
+  group = Config.augr,
+  pattern = "BlinkCmpMenuOpen",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+autocmd("User", {
+  group = Config.augr,
+  pattern = "BlinkCmpMenuClose",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
 
 -- sidekick.nvim
 add({ Config.gh("folke/sidekick.nvim") })
@@ -49,16 +52,16 @@ require("sidekick").setup()
 
 require("sidekick.nes").disable()
 
--- map({ "i", "n" }, "<Tab>", function()
---   -- If there is a next edit, jump to it or apply it.
---   if require("sidekick").nes_jump_or_apply() then return "" end
---
---   -- Fallback to a literal Tab.
---   return "<tab>"
--- end, {
---   expr = true,
---   desc = "Goto/Apply Next Edit Suggestion",
--- })
+map({ "i", "n" }, "<Tab>", function()
+  -- If there is a next edit, jump to it or apply it.
+  if require("sidekick").nes_jump_or_apply() then return "" end
+
+  -- Fallback to a literal Tab.
+  return "<tab>"
+end, {
+  expr = true,
+  desc = "Goto/Apply Next Edit Suggestion",
+})
 
 map({ "n", "t", "i", "x" }, "<C-.>", function()
   require("sidekick.cli").focus()
